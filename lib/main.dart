b9115261d0411a9953a2b07403304b8003c46013
lluvvc <jimbo21xc30@gmail.com>
2021-05-20 12:31:12 +0800

    Complete Storage  Feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'qrcode_scanner.dart';
 import 'http_data.dart';
+import 'screenInput.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -10,22 +12,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        title: Text('Covid Autofill'),
-      ),
-      body: HomePage(),
-    ));
+          appBar: AppBar(
+            title: Text('Covid Autofill'),
+          ),
+          body: HomePage(),
+        ),
+        routes: <String, WidgetBuilder>{
+          "/ScreenInput": (BuildContext context) => new ScreenInput()
+        });
   }
 }
 
 class HomePage extends StatelessWidget {
+  final TextEditingController myController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: ElevatedButton(
-      child: Text('QEcode Scanner'),
-      onPressed: request,
-    ));
+        child: Column(children: <Widget>[
+      ElevatedButton(
+        child: Text('QEcode Scanner'),
+        onPressed: request,
+      ),
+      // entry point of Screen Input Try
+      ElevatedButton(
+          child: Text('Change to Screen Input'),
+          onPressed: () {
+            Navigator.of(context).pushNamed("/ScreenInput");
+          })
+    ]));
   }
-
 }
---
+++ lib/screenInput.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'saveJson.dart';
+
+class ScreenInput extends StatelessWidget {
+  final TextEditingController nameController = new TextEditingController();
+  final TextEditingController phoneController = new TextEditingController();
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController idController = new TextEditingController();
+  final content = new ContentStorage();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+            title: new Text("Second Page"), backgroundColor: Colors.deepOrange),
+        body: new Container(
+            child: Center(
+                child: Flexible(
+                    child: Column(children: <Widget>[
+          TextField(
+              decoration: InputDecoration(
+                labelText: 'Name',
+                hintText: '本名',
+              ),
+              controller: nameController),
+          TextField(
+              decoration: InputDecoration(
+                labelText: 'Phone',
+                hintText: '電話號碼',
+              ),
+              controller: phoneController),
+          TextField(
+              decoration: InputDecoration(
+                labelText: 'Email',
+                hintText: '電子郵件',
+              ),
+              controller: emailController),
+          TextField(
+              decoration: InputDecoration(
+                labelText: 'ID Number',
+                hintText: '身分證字號',
+              ),
+              controller: idController),
+          TextButton(
+            child: Text('印出輸入框內容'),
+            onPressed: btnEvent,
+          ),
+          TextButton(
+            child: Text('印出儲存內容'),
+            onPressed: btnEvent2,
+          )
+        ])))));
+  }
+
+  void btnEvent() {
+    Content info = new Content(nameController.text, phoneController.text,
+        emailController.text, idController.text); // new storage object
+
+    content.writeContent(info); // Write FIle
+  }
+
+  void btnEvent2() async {
+    final info = await content.readContent(); // Read File
+    print(info.name);
+    print(info.phone);
+    print(info.email);
+    print(info.id);
+  }
+}
