d2b3d8a028f604cb9e2fbd2b8cc86779ee9fbeb1
KaiChen1008 <1038790@gmail.com>
2021-05-22 17:21:31 +0800

    open form webpage

---
+++ lib/open_form_ver2.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+// open the form in outside app (e.g. chrome, safari...)
+
+class FormView extends StatefulWidget {
+  @override
+  _FormView createState() => _FormView();
+}
+
+class _FormView extends State<FormView> {
+  final GlobalKey webViewKey = GlobalKey();
+  String url =
+      "https://docs.google.com/forms/d/e/1FAIpQLScw30Eq9oRONqVv_tLolp0BubC8LfIMllCgffOoLbD7MJpuBg/viewform?emailAddress=123@gmail.com&entry.770857854=%E5%8A%89%E8%B1%AA%E5%B2%A1&entry.1613430569=1";
+
+  late PullToRefreshController pullToRefreshController;
+  double progress = 0;
+  final urlController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: _launchURL, 
+            child: Text('go')
+          ),
+        )
+    );
+  }
+}
+
+_launchURL() async {
+  const url =
+      "https://docs.google.com/forms/d/e/1FAIpQLScw30Eq9oRONqVv_tLolp0BubC8LfIMllCgffOoLbD7MJpuBg/viewform?emailAddress=123@gmail.com&entry.770857854=%E5%8A%89%E8%B1%AA%E5%B2%A1&entry.1613430569=1";
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'could not launch $url';
+  }
+}
